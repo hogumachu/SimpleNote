4023fa0ca2c9a61ebc7d9b6d959fe69da32f4e6e
hogumachu <castle_jun@daum.net>
2024-04-15 22:41:05 +0900

    feat: FolderDetail 추가

---
+++ SimpleNote/Features/FolderDetail/FolderDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  FolderDetailView.swift
+//  SimpleNote
+//
+//  Created by 홍성준 on 4/15/24.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct FolderDetailView: View {
+  
+  let store: StoreOf<FolderDetailViewStore>
+  
+  var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      
+    }
+  }
+  
+}
---
+++ SimpleNote/Features/FolderDetail/FolderDetailViewStore.swift
@@ -0,0 +1,14 @@
+//
+//  FolderDetailViewStore.swift
+//  SimpleNote
+//
+//  Created by 홍성준 on 4/15/24.
+//
+
+import ComposableArchitecture
+import Foundation
+
+@Reducer
+struct FolderDetailViewStore {
+  
+}
