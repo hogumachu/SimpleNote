203e468435e46e23683a37e120d93df337e15435
hogumachu <castle_jun@daum.net>
2024-04-15 20:29:11 +0900

    feat: Database 추가

---
+++ SimpleNote/Database/Database.swift
@@ -0,0 +1,39 @@
+//
+//  Database.swift
+//  SimpleNote
+//
+//  Created by 홍성준 on 4/15/24.
+//
+
+import Dependencies
+import SwiftData
+
+struct Database {
+  var context: () throws -> ModelContext
+}
+
+extension Database: DependencyKey {
+  static let liveValue = Database(context: {
+    do {
+      let schema = Schema([])
+      let configuration = ModelConfiguration(
+        schema: schema,
+        isStoredInMemoryOnly: false
+      )
+      let container = try ModelContainer(
+        for: schema,
+        configurations: [configuration]
+      )
+      return ModelContext(container)
+    } catch {
+      throw error
+    }
+  })
+}
+
+extension DependencyValues {
+  var database: Database {
+    get { self[Database.self] }
+    set { self[Database.self] = newValue }
+  }
+}
