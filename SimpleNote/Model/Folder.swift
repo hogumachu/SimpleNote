7af066095f0853b1372ad5acffb90edd68833d4f
hogumachu <castle_jun@daum.net>
2024-04-15 20:35:03 +0900

    feat: Folder model 추가

---
+++ SimpleNote/Model/Folder.swift
@@ -0,0 +1,24 @@
+//
+//  Folder.swift
+//  SimpleNote
+//
+//  Created by 홍성준 on 4/15/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Folder {
+  
+  @Attribute(.unique) var id: UUID
+  var title: String
+  var hexColor: String
+  
+  init(id: UUID, title: String, hexColor: String) {
+    self.id = id
+    self.title = title
+    self.hexColor = hexColor
+  }
+  
+}
