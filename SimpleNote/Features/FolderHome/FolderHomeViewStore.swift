6e78299f7eef6e1e82ac84f7e865c701f0854079
hogumachu <castle_jun@daum.net>
2024-04-16 21:01:48 +0900

    feat: Folder Create 추가

--- SimpleNote/Features/FolderHome/FolderHomeViewStore.swift
+++ SimpleNote/Features/FolderHome/FolderHomeViewStore.swift
@@ -13,14 +13,20 @@ struct FolderHomeViewStore: Reducer {
   
   @ObservableState
   struct State: Equatable {
+    @Presents var folderCreate: FolderCreateViewStore.State?
     var path = StackState<FolderDetailViewStore.State>()
-    var folders = [Folder]()
+    var folders = IdentifiedArrayOf<Folder>()
   }
   
   enum Action {
     case path(StackAction<FolderDetailViewStore.State, FolderDetailViewStore.Action>)
+    case onAppeared
     case addButtonTapped
+    case setFolders(IdentifiedArrayOf<Folder>)
+    case folderCreate(PresentationAction<FolderCreateViewStore.Action>)
   }
+  
+  @Dependency(\.folderDatabase) var database
   
   var body: some ReducerOf<Self> {
     Reduce { state, action in
@@ -28,9 +34,31 @@ struct FolderHomeViewStore: Reducer {
       case .path:
         return .none
         
+      case .onAppeared:
+        return .run { @MainActor send in
+          try send(.setFolders(.init(uniqueElements: database.fetchAll())))
+        }
+        
       case .addButtonTapped:
+        state.folderCreate = FolderCreateViewStore.State(title: "", hexColor: "F05138")
+        return .none
+        
+      case let .setFolders(folders):
+        state.folders = folders
+        return .none
+        
+      case let .folderCreate(.presented(.delegate(.create(folder)))):
+        return .run { send in
+          try database.add(folder)
+          await send(.onAppeared)
+        }
+        
+      case .folderCreate:
         return .none
       }
+    }
+    .ifLet(\.$folderCreate, action: \.folderCreate) {
+      FolderCreateViewStore()
     }
     .forEach(\.path, action: \.path) {
       FolderDetailViewStore()
