ec8cc8dcb9836379600b4ac8f3b7726562d3ed3f
hogumachu <castle_jun@daum.net>
2024-04-18 23:34:41 +0900

    chore: color style 추가

---
+++ SimpleNote/Extensions/Color+.swift
@@ -0,0 +1,15 @@
+//
+//  Color+.swift
+//  SimpleNote
+//
+//  Created by 홍성준 on 4/18/24.
+//
+
+import SwiftUI
+
+extension Color {
+  
+  static let secondarySystemBackground = Color(uiColor: UIColor.secondarySystemBackground)
+  static let tertiarySystemBackground = Color(uiColor: UIColor.tertiarySystemBackground)
+  
+}
