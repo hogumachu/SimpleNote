573369752aa8093d5161b1d64dbe937d68888fb1
hogumachu <castle_jun@daum.net>
2024-04-19 13:17:13 +0900

    chore: SwiftDate region 설정 추가

---
+++ SimpleNote/App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleNote
+//
+//  Created by 홍성준 on 4/19/24.
+//
+
+import UIKit
+import SwiftDate
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    appSetup()
+    return true
+  }
+  
+}
+
+private extension AppDelegate {
+  
+  func appSetup() {
+    SwiftDate.defaultRegion = .local
+  }
+  
+}
