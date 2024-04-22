7a1f4cba78a002ba7ac708247b91be0a6e27ad75
hogumachu <castle_jun@daum.net>
2024-04-23 01:58:10 +0900

    chore: bundle resource 추가

---
+++ SimpleNote/Feature/Sources/BaseFeature/Extensions/ImageResource+.swift
@@ -0,0 +1,34 @@
+//
+//  ImageResource+.swift
+//
+//
+//  Created by 홍성준 on 4/23/24.
+//
+
+import SwiftUI
+
+public extension ImageResource {
+  
+  static let ArrowLeft: ImageResource = .arrowLeft
+  static let CalendarDots: ImageResource = .calendarDots
+  static let CalendarDotsFill: ImageResource = .calendarDotsFill
+  static let CaretRight: ImageResource = .caretRight
+  static let CheckCircleFill: ImageResource = .checkCircleFill
+  static let Circle: ImageResource = .circle
+  static let Folder: ImageResource = .folder
+  static let FolderFill: ImageResource = .folderFill
+  static let Folders: ImageResource = .folders
+  static let FoldersFill: ImageResource = .foldersFill
+  static let GearFill: ImageResource = .gearFill
+  static let House: ImageResource = .house
+  static let HouseFill: ImageResource = .houseFill
+  static let MagnifyingGlass: ImageResource = .magnifyingGlass
+  static let PencilSimple: ImageResource = .pencilSimple
+  static let PencilSimpleFill: ImageResource = .pencilSimpleFill
+  static let Plus: ImageResource = .plus
+  static let PlusCircleFill: ImageResource = .plusCircleFill
+  static let Trash: ImageResource = .trash
+  static let TrashFill: ImageResource = .trashFill
+  static let Xmark: ImageResource = .X
+  
+}
