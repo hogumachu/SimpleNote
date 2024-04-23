f309b0c732598edbd9434f1022334211b0c123e9
hogumachu <castle_jun@daum.net>
2024-04-23 16:47:25 +0900

    feat: setting 의존성 주입 추가

---
+++ SimpleNote/App/CompositionRoot.swift
@@ -0,0 +1,17 @@
+//
+//  CompositionRoot.swift
+//  SimpleNote
+//
+//  Created by 홍성준 on 4/23/24.
+//
+
+import SettingFeatureImp
+import UIFeatureKit
+
+enum CompositionRoot {
+  
+  static func assemble() {
+    SettingAssemble.assemble()
+  }
+  
+}
